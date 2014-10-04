5ad44c289ea60e9a4052cca3d3c5dab430cc467b
Amir Geva <amirgeva@gmail.com>
2014-10-04 17:53:02 +0300

    Added settings to control C++11 compilation

--- gen/build_settings.py
+++ gen/build_settings.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'uis/build_settings.ui'
 #
-# Created: Fri Oct  3 00:29:25 2014
+# Created: Sat Oct  4 17:45:44 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
