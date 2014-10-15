c754f7f5f20354cba504747a1774a164bbd4e69b
Amir Geva <amirgeva@gmail.com>
2014-10-15 20:34:39 +0000

    Added async executes for non blocking builds

--- utils.py
+++ utils.py
@@ -78,8 +78,57 @@ def run(dir,cmd,*args):
 
 def call(dir,cmd,*args):
     return runcmd(dir,[cmd]+list(args)).communicate()
-    
+
+class AsyncExecute:
+    def __init__(self,output,dir,cmdlist):
+        self.output=output
+        self.pdone=False
+        self.text=[]
+        self.act=1
+        self.process=subprocess.Popen(cmdlist, shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE,cwd=dir)
+        
+    def poll(self):
+        if self.pdone and self.act==0:
+            return True
+        reads=[self.process.stdout.fileno(),self.process.stderr.fileno()]
+        self.act=0
+        rc=select.select(reads,[],[])
+        for fd in rc[0]:
+            if fd==self.process.stdout.fileno():
+                line=self.process.stdout.readline().strip()
+                appendLine(self.output,line)
+                self.text.append(line)
+                if len(line)>0:
+                    self.act=self.act+1
+            if fd==self.process.stderr.fileno():
+                line=self.process.stderr.readline().strip()
+                appendLine(self.output,line)
+                self.text.append(line)
+                if len(line)>0:
+                    self.act=self.act+1
+        if self.process.poll()!=None:
+            self.pdone=True
+        if self.pdone and self.act==0:
+            return True
+        return False
+
+async_executes=[]
+        
 def execute(output,dir,cmd,*args):
+    cmdlist=[cmd]+list(args)
+    async_executes.append(AsyncExecute(output,dir,cmdlist))
+    
+def pollAsync():
+    n=len(async_executes)
+    for i in xrange(0,n):
+        ae=async_executes[i]
+        if ae.poll():
+            del async_executes[i]
+            return pollAsync()
+    return n>0
+        
+
+def old_execute(output,dir,cmd,*args):
     output.clear()
     cmdlist=[cmd]+list(args)
     text=[]
