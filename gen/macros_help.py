af6c5ba8fc969a5956ce86379b74b405ff89560e
Amir Geva <amirgeva@gmail.com>
2014-10-18 15:29:45 +0000

    Added templates macros help

---
+++ gen/macros_help.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'uis/macros_help.ui'
+#
+#
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(400, 300)
+        Dialog.buttonBox = QtGui.QDialogButtonBox(Dialog)
+        Dialog.buttonBox.setGeometry(QtCore.QRect(50, 260, 341, 32))
+        Dialog.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        Dialog.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        Dialog.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        Dialog.macrosTable = QtGui.QTableWidget(Dialog)
+        Dialog.macrosTable.setGeometry(QtCore.QRect(10, 10, 381, 241))
+        Dialog.macrosTable.setObjectName(_fromUtf8("macrosTable"))
+        Dialog.macrosTable.setColumnCount(0)
+        Dialog.macrosTable.setRowCount(0)
+        Dialog.macrosTable.horizontalHeader().setVisible(True)
+        Dialog.macrosTable.horizontalHeader().setStretchLastSection(True)
+        Dialog.macrosTable.verticalHeader().setVisible(False)
+
+        self.retranslateUi(Dialog)
+        QtCore.QObject.connect(Dialog.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), Dialog.accept)
+        QtCore.QObject.connect(Dialog.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Macros Help", None))
+
