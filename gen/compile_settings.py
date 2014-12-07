31faefb5e3bcb02cddce56fb67fb7292d22517d0
Amir Geva <amirgeva@gmail.com>
2014-12-07 14:47:11 +0200

    Updated generation tool version

--- gen/compile_settings.py
+++ gen/compile_settings.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'uis/compile_settings.ui'
 #
 #
-#      by: PyQt4 UI code generator 4.10.4
+#      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
 
