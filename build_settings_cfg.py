6b8189341a2a61f4c03b592177b34f599a7ad1a6
Amir Geva <amirgeva@gmail.com>
2015-12-16 22:46:08 +0200

    Updated build settings options

--- build_settings_cfg.py
+++ build_settings_cfg.py
@@ -2,22 +2,23 @@
 # are 'name' and 'title'
 # The third member can be either:
 #   1.  a string containing pipe delimited pulldown options
-#   2.  'CB' for a checkbox, followed by a boolean member for the default
-#   3.  'STR' for a string value, followed by the string default
+#   2.  'CB' for a checkbox, followed by a boolean member for the default, and the flag that is used if true
+#   3.  'STR' for a single-line string value, followed by the string default
+#   4.  'EDIT' for a multi-line string value, followed by the string default
 
 tabs={
     'Compile':[
         ('BUILD_OPT','Optimization','-O0|-O1|-O2|-O3','-O2'),
-        ('BUILD_WARN','Warnings','Default|None (-w)|All (-Wall)','Default'),
-        ('BUILD_PEDANTIC','Pedantic','CB',False),
-        ('BUILD_WARNERR','Warning as errors','CB',False),
-        ('BUILD_CPP11','C++11','CB',True)
+        ('COMPILE_WARN','Warnings','Default|None (-w)|All (-Wall)','Default'),
+        ('COMPILE_PEDANTIC','Pedantic','CB',False,'-pedantic-errors'),
+        ('COMPILE_WARNERR','Warning as errors','CB',False,'-Werror'),
+        ('COMPILE_CPP11','Standard','Default|-std=c++0x|-std=c++11','-std=c++11')
     ],
     'Link':[
     ],
     'Advanced':[
-        ('BUILD_CUSTOM_COMPILE','Custom Compile Flags','EDIT',''),
-        ('BUILD_CUSTOM_LINK','Custom Link Flags','EDIT','')
+        ('COMPILE_CUSTOM','Custom Compile Flags','EDIT',''),
+        ('LINK_CUSTOM','Custom Link Flags','EDIT','')
     ]
 }
 
