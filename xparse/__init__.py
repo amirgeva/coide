07cf4de39019cea27ab39556f913a79939869242
Amir Geva <amirgeva@gmail.com>
2015-12-20 12:22:04 +0200

    ignore bad variable output

--- xparse/__init__.py
+++ xparse/__init__.py
@@ -5,6 +5,8 @@ from xlex import TokenizerException,EndOfText
 def parse(text):
     #print "Parsing: '{}'".format(text)
     try:
+        if text.find('error reading variable:')>=0:
+            return None
         return Parser(text).root
     except TokenizerException,e:
         pass
