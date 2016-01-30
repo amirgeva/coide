d9fce5da6e1da4d5e34bc11fea33e49fe69fe6a9
Amir Geva <amirgeva@gmail.com>
2016-01-31 00:12:42 +0200

    Scan and report SCM pending changes

---
+++ scm/__init__.py
@@ -0,0 +1,15 @@
+from os.path import dirname, basename, isfile
+import importlib
+import glob
+names = glob.glob(dirname(__file__)+"/*.py")
+names = [ '.'+basename(f)[:-3] for f in names if isfile(f) and basename(f)!='__init__.py']
+modules=[]
+for name in names:
+    modules.append(importlib.import_module(name,'scm'))
+
+def scan(root):
+    for m in modules:
+        res=m.scan(root)
+        if res:
+            return res
+    return None
