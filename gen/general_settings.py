588c857350de857e233b3151102a81b1e74dd2ac
Amir Geva <amirgeva@gmail.com>
2016-11-22 16:18:11 +0200

    auto-gen

--- gen/general_settings.py
+++ gen/general_settings.py
@@ -3,7 +3,6 @@
 # Form implementation generated from reading ui file 'uis/general_settings.ui'
 #
 #
-#      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -36,8 +35,11 @@ class Ui_EditorSettingsDialog(object):
         EditorSettingsDialog.sortFilesCB.setGeometry(QtCore.QRect(10, 20, 321, 27))
         EditorSettingsDialog.sortFilesCB.setObjectName(_fromUtf8("sortFilesCB"))
         EditorSettingsDialog.clearCacheButton = QtGui.QPushButton(EditorSettingsDialog)
-        EditorSettingsDialog.clearCacheButton.setGeometry(QtCore.QRect(20, 60, 85, 27))
+        EditorSettingsDialog.clearCacheButton.setGeometry(QtCore.QRect(20, 90, 85, 27))
         EditorSettingsDialog.clearCacheButton.setObjectName(_fromUtf8("clearCacheButton"))
+        EditorSettingsDialog.customPrinters = QtGui.QCheckBox(EditorSettingsDialog)
+        EditorSettingsDialog.customPrinters.setGeometry(QtCore.QRect(10, 50, 201, 20))
+        EditorSettingsDialog.customPrinters.setObjectName(_fromUtf8("customPrinters"))
 
         self.retranslateUi(EditorSettingsDialog)
         QtCore.QObject.connect(EditorSettingsDialog.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), EditorSettingsDialog.accept)
@@ -48,4 +50,5 @@ class Ui_EditorSettingsDialog(object):
         EditorSettingsDialog.setWindowTitle(_translate("EditorSettingsDialog", "Editor Settings", None))
         EditorSettingsDialog.sortFilesCB.setText(_translate("EditorSettingsDialog", "Sort by File name", None))
         EditorSettingsDialog.clearCacheButton.setText(_translate("EditorSettingsDialog", "Clear Cache", None))
+        EditorSettingsDialog.customPrinters.setText(_translate("EditorSettingsDialog", "Use custom gdb printers", None))
 
