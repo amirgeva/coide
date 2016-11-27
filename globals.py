dbf58ceeccf917aedcd1e00d5b9868650ff768a0
Amir Geva <amirgeva@gmail.com>
2016-11-27 10:22:44 +0200

    Generalized source file extensions

--- globals.py
+++ globals.py
@@ -2,3 +2,20 @@
 
 mw=None
 dev=False
+
+# It's important to have .cpp before .c  for replace function when creating .o names
+src_exts=['.cpp','.c']
+hdr_exts=['.hpp','.h']
+
+def is_src_ext(path):
+    for e in src_exts:
+        if path.endswith(e):
+            return True
+    return False
+    
+def is_header(path):
+    for e in hdr_exts:
+        if path.endswith(e):
+            return True
+    return False
+    
